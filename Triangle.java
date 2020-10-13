462e8ededbc21664813d414e54afb644d281d1b6
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-13 14:02:44 -0500

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,58 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import java.lang.Math.*;
+
+public class Triangle
+{
+	private int sideA, sideB, sideC;
+	private double perimeter;
+	private double theArea;
+
+	public Triangle()
+	{
+	   setSides(0,0,0);
+	   perimeter=0;
+	   theArea=0;
+	}
+
+	public Triangle(int a, int b, int c)
+	{
+
+
+	}
+
+
+	public void setSides(int a, int b, int c)
+	{
+
+
+	}
+
+	public void calcPerimeter( )
+	{
+
+
+	}
+
+	public double calcArea( )
+	{
+		double s;
+
+
+	}
+
+	public void print( )
+	{
+
+               // Call the method calcArea and print the result
+
+		System.out.println("\n\n");
+	}
+}
